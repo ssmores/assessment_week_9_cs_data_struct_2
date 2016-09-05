c9a36005ffc45ac504fa65d32a1b022048e16e16
Stefanie Moy <ssmores@hotmail.com>
2016-09-05 02:05:11 +0000

    Finished bubble sorting.

---
+++ sorting.py
@@ -0,0 +1,65 @@
+#Sorting
+
+def bubble_sort(lst):
+    """returns a sorted list using a optimized bubble sort algorithm
+    i.e. using a variable to track if there hasn't been a swap
+        >>> bubble_sort([3, 5, 7, 2, 4, 1])
+        [1, 2, 3, 4, 5, 7]
+    """
+
+    
+    # The outer loop makes sure the list moves through the list after the inner sort happens.
+    for i in range(len(lst) - 1):
+        swapped = False
+        # The inner loop gets the highest value to the end.
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                swapped = True
+        if not swapped:
+            break
+            
+    return lst 
+
+
+def merge_lists(list1, list2):
+    """Given two sorted lists of integers, returns a single sorted list containing all
+    integers in the input lists
+    >>> merge_lists([1, 3, 9], [4, 7, 11])
+    [1, 3, 4, 7, 9, 11]
+    """
+
+    new_list = []
+
+    while list1 or list2:
+        if 
+
+
+##########ADVANCED##########
+def merge_sort(lst):
+    """
+    Given a list, returns a sorted version of that list.
+    Finish the merge sort algorithm by writing another function that
+    that takes in a single unsorted list of integers and uses recursion and the 'merge_lists'
+    function you already wrote to return a new sorted list containing all integers from
+    thin input list. In other words, the new function should sort a list using merge_lists
+    and recursion.
+    >>> merge_sort([6, 2, 3, 9, 0, 1])
+    [0, 1, 2, 3, 6, 9]
+    """
+    pass
+
+
+
+
+#####################################################################
+# END OF ASSIGNMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
